f5bcadbdd7f990c3c127c69ab4a48b37eeab70d7
yar-24 <akhyar24.354@gmail.com>
2023-03-04 12:03:03 +0700

    make pass

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import './index.css';
-// import App from './App';
-// import store from './states/store';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import store from './states/store';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <Provider store={store}>
-//     <BrowserRouter>
-//       <React.StrictMode>
-//         <App />
-//       </React.StrictMode>
-//     </BrowserRouter>
-//   </Provider>,
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
+);
